12dae7bb48b121b31eee6e3d157908fef7618f04
Iñigo Herrero <inigoortega2000@gmail.com>
2018-11-03 19:20:37 +0100

    Revert "V.1.2"
    This reverts commit e4986e4c655e098e30151668833cd0ec81bd35da.

--- gameOfLife/script.js
+++ gameOfLife/script.js
@@ -11,7 +11,7 @@ function startLife() {
 	loop();
 
 	function loop() {
-		dibujarGrid();
+		drawGrid();
 		actualizarGrid();
 		requestAnimationFrame(loop);
 	}
@@ -32,7 +32,7 @@ function startLife() {
 		}
 	}
 
-	function dibujarGrid() { //dibuja los valores de la tabla en el canvas
+	function drawGrid() { //dibuja los valores de la tabla en el canvas
 		ctx.clearRect(0, 0, 400, 400);
 
 		for(var j = 1; j < gridHeight; j++) {
@@ -72,12 +72,14 @@ function startLife() {
 
 				} else if(grid[j][k] === 1) {
 					switch(totCel) {
+						case 0:
+						case 1:
+							copiaGrid[j][k] = 0;
+							break
 						case 2:
 						case 3:
 							copiaGrid[j][k] = 1;
-							break;
-						case 0:
-						case 1:
+							break
 						case 4:
 						case 5:
 						case 6:
